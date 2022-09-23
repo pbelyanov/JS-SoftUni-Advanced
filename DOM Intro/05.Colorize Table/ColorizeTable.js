c9839a5cb0cd5e5fa3517fea75f1ed5b28105df6
pbelyanov <p.belyanov@yahoo.com>
2022-09-23 15:40:16 +0300

    Update ColorizeTable.js

--- DOM Intro/05.Colorize Table/ColorizeTable.js
+++ DOM Intro/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,10 @@
 function colorize() {
-    // TODO
+    let tableRow = document.querySelectorAll('table tr');
+    console.log(tableRow)
+
+    for (let i = 0; i < tableRow.length; i++) {
+        if (i % 2 !== 0) {
+            document.querySelectorAll('table tr')[i].style = 'background-color: teal'
+        }
+    }
 }
