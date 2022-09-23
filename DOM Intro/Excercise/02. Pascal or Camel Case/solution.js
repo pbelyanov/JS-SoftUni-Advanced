04e58af99f50b1178083774938a0cf47a497e4bc
pbelyanov <p.belyanov@yahoo.com>
2022-09-24 00:59:54 +0300

    main

--- DOM Intro/Excercise/02. Pascal or Camel Case/solution.js
+++ DOM Intro/Excercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,37 @@
 function solve() {
-  //TODO...
+  let input = document.getElementById('text').value;
+  let currentCase = document.getElementById('naming-convention').value;
+  input = input.toLowerCase()
+  let result = input.split(' ');
+  let finalResult = [];
+  let checkedWord = ''
+  let checkedWordFinal = ''
+
+  switch (currentCase) {
+    case 'Camel Case':
+      finalResult.push(result[0])
+      for (let i = 1; i < result.length; i++) {
+        checkedWord = result[i];
+        checkedWordFinal = checkedWord.charAt(0).toUpperCase() + checkedWord.slice(1);
+        finalResult.push(checkedWordFinal)
+      }
+      result.join('')
+      break;
+    case 'Pascal Case':
+      for (let i of result) {
+        checkedWord = i;
+        checkedWordFinal = checkedWord.charAt(0).toUpperCase() + checkedWord.slice(1);
+        finalResult.push(checkedWordFinal)
+      }
+      result.join('')
+      break;
+    default:
+      finalResult = 'Error!'
+      document.getElementById('result').textContent = finalResult;
+      break;
+  }
+
+  if (currentCase === 'Camel Case' || currentCase === 'Pascal Case') {
+    document.getElementById('result').textContent = finalResult.join('')
+  }
 }
