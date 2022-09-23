268f9cbbb0a73726095857481e776f10ad29ef31
pbelyanov <p.belyanov@yahoo.com>
2022-09-23 15:55:36 +0300

    Update SumTable.js

--- DOM Intro/06.Sum Table/SumTable.js
+++ DOM Intro/06.Sum Table/SumTable.js
@@ -1,3 +1,11 @@
 function sumTable() {
+    let result = 0;
+    let numLines = document.querySelectorAll('td')
 
+    for (let i = 0; i < numLines.length - 2; i++) {
+        if (i % 2 !== 0) {
+            result += Number(document.querySelectorAll('td')[i].textContent)
+        }
+    }
+    document.getElementById('sum').textContent = result
 }
