14eb810def865ea254246028ec1256960ed6e376
pbelyanov <p.belyanov@yahoo.com>
2022-09-25 01:58:32 +0300

    main

--- DOM Intro/Excercise/05. Table - Search Engine/solution.js
+++ DOM Intro/Excercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,32 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
+   let list = document.querySelectorAll('tbody tr td');
 
    function onClick() {
-      //   TODO:
+      let counter = 0;
+      let i = 0;
 
+
+      let searchedWord = document.getElementById('searchField').value;
+
+      let parent = document.querySelectorAll('tbody tr');
+      for (let i = 0; i < parent.length; i++) {
+         parent[i].classList.remove('select')
+      }
+
+
+      for (let line of list) {
+         let result = line.textContent.includes(searchedWord)
+         if (result === true) {
+            let currentRow = document.querySelectorAll('tbody tr')[Math.floor(i / 3)];
+            currentRow.classList.add('select')
+            console.log(currentRow)
+         }
+         counter++;
+         i++
+
+      }
+
+      document.getElementById('searchField').value = ''
    }
 }
