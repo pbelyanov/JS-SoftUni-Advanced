86147b11b8639a5b82a6627992fd49144a8d893a
pbelyanov <p.belyanov@yahoo.com>
2022-09-24 13:03:30 +0300

    Update accordion.js

--- DOM Intro/Excercise/03. Accordion/accordion.js
+++ DOM Intro/Excercise/03. Accordion/accordion.js
@@ -1,3 +1,13 @@
 function toggle() {
-    console.log('TODO:...');
+
+    let buttonSwitch = document.getElementsByClassName('button')[0].textContent;
+
+    if (buttonSwitch === 'More') {
+        document.getElementById('extra').style = 'display:block'
+        document.getElementsByClassName('button')[0].textContent = 'Less'
+    } else {
+        document.getElementById('extra').style = 'display:none'
+        document.getElementsByClassName('button')[0].textContent = 'More'
+    }
+
 }
