9201fc876e47f690002173117be39a8b13492ef1
pbelyanov <p.belyanov@yahoo.com>
2022-09-24 14:01:50 +0300

    main

--- DOM Intro/Excercise/04. Search in List/search.js
+++ DOM Intro/Excercise/04. Search in List/search.js
@@ -1,3 +1,22 @@
 function search() {
-   // TODO
+
+   let list = document.querySelectorAll('ul li');
+   let searchedWord = document.getElementById('searchText').value;
+   let i = 0;
+   let counter = 0;
+
+
+   for (let line of list) {
+      document.querySelectorAll('ul li')[i].setAttribute('style', 'font-weight: normal;text-decoration: none;');
+
+      let result = line.textContent.includes(searchedWord)
+
+      if (result === true) {
+         counter++;
+         document.querySelectorAll('ul li')[i].setAttribute('style', 'font-weight: bold;text-decoration: underline;')
+      }
+      i++
+   }
+   document.getElementById('result').textContent = `${counter} matches found`
+
 }
