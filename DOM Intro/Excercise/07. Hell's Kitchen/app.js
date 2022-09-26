cfc7c0736d4db2a884f357fbf2172926b8f63765
pbelyanov <p.belyanov@yahoo.com>
2022-09-26 23:34:17 +0300

    main

--- DOM Intro/Excercise/07. Hell's Kitchen/app.js
+++ DOM Intro/Excercise/07. Hell's Kitchen/app.js
@@ -1,8 +1,42 @@
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
-   function onClick () {
-      //   TODO:
-      
+   function onClick() {
+      let input = JSON.parse(document.getElementsByTagName('textarea')[0].value);
+      let allRestaurants = {};
+      let bestAverageSalary = 0;
+      let bestRestaurantName = '';
+
+      for (let lines of input) {
+         let [resturantName, workers] = lines.split(' - ');
+         let indvidualWorkers = workers.split(', ')
+
+         for (let worker of indvidualWorkers) {
+            let [workerName, salary] = worker.split(' ');
+
+            if (!allRestaurants.hasOwnProperty(resturantName)) {
+               allRestaurants[resturantName] = {};
+            }
+            allRestaurants[resturantName][workerName] = Number(salary);
+         }
+
+         let entries = Object.entries(allRestaurants);
+
+         for (let [name, workers] of entries) {
+            let salaries = Object.values(workers);
+            let totalSalariesPaid = 0;
+
+            for (const salary of salaries) {
+               totalSalariesPaid += salary;
+            }
+
+            let averageSalary = totalSalariesPaid / salaries.length;
+
+            if (averageSalary > bestAverageSalary) {
+               bestAverageSalary = averageSalary;
+               bestRestaurantName = name;
+            }
+         }
+      }
    }
 }
