c9ec83f36fc45f03cede3c900868d44d28477c1b
pbelyanov <p.belyanov@yahoo.com>
2022-09-30 13:50:55 +0300

    main

--- DOM Manipulation and Events/Excercise/03. Locked-Profile/app.js
+++ DOM Manipulation and Events/Excercise/03. Locked-Profile/app.js
@@ -1,3 +1,28 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let button = Array.from(document.getElementsByTagName('button'))
+
+    for (let line of button) {
+        line.addEventListener('click', showHidden)
+    }
+
+    function showHidden(event) {
+        let checked = event.currentTarget.parentNode.querySelectorAll('input[value="unlock"]')[0].checked;
+        let div = event.currentTarget.parentNode.getElementsByTagName('div')[0];
+
+
+        if (event.currentTarget.textContent === 'Hide it!' && checked === true) {
+            div.style = 'display: none'
+            event.currentTarget.textContent = 'Show more'
+            return;
+        }
+
+
+        if (checked) {
+            div.style = 'display: block'
+            event.currentTarget.textContent = 'Hide it!'
+            return;
+        }
+
+    }
+
 }
