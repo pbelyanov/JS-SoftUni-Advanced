1739f75889bb351a2e9db0117517f0bf48f66d79
pbelyanov <p.belyanov@yahoo.com>
2022-11-15 17:45:06 +0200

    main

--- JS Applications/Data and Authentication/05.Fisher-Game/src/app.js
+++ JS Applications/Data and Authentication/05.Fisher-Game/src/app.js
@@ -1,1 +1,14 @@
 console.log('TODO:// Implement Home functionality');
+
+const buttonLogout = document.getElementById('logout');
+buttonLogout.addEventListener('click', async event => {
+
+    let data = await fetch('http://localhost:3030/users/logout', {
+        method: 'get',
+        headers: {
+            'X-Authorization': `${localStorage.getItem('authToken')}`
+        }
+    });
+
+    localStorage.clear();
+})
--- JS Applications/Data and Authentication/05.Fisher-Game/src/register.js
+++ JS Applications/Data and Authentication/05.Fisher-Game/src/register.js
@@ -1,1 +1,40 @@
-console.log('TODO:// Implement Register functionality');
+const formElement = document.getElementById('register-form');
+
+formElement.addEventListener('submit', submitRegister)
+
+
+async function submitRegister(event) {
+    event.preventDefault();
+    const dataForm = new FormData(event.target);
+
+    const data = Object.fromEntries(dataForm.entries());
+
+    let dataToPost = {};
+    dataToPost.email = data.email;
+    dataToPost.password = data.password;
+
+    let registered = await fetch('http://localhost:3030/users/register', {
+        method: 'post',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(dataToPost)
+    })
+    let response = await registered.json()
+
+    const authToken = response.accessToken;
+    localStorage.setItem('authToken', authToken);
+}
+
+const buttonLogout = document.getElementById('logout');
+buttonLogout.addEventListener('click', async event => {
+
+    let data = await fetch('http://localhost:3030/users/logout', {
+        method: 'get',
+        headers: {
+            'X-Authorization': `${localStorage.getItem('authToken')}`
+        }
+    });
+
+    localStorage.clear();
+})
